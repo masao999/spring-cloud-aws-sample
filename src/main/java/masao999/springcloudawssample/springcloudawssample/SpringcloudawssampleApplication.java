5dae9521565c91fae84f880d07e50cce6d0fc6cc
masao999 <42270841+masao999@users.noreply.github.com>
2023-10-04 10:14:32 +0900

    新規追加

---
+++ src/main/java/masao999/springcloudawssample/springcloudawssample/SpringcloudawssampleApplication.java
@@ -0,0 +1,13 @@
+package masao999.springcloudawssample.springcloudawssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudawssampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudawssampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/masao999/springcloudawssample/springcloudawssample/SpringcloudawssampleApplicationTests.java
@@ -0,0 +1,13 @@
+package masao999.springcloudawssample.springcloudawssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudawssampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
